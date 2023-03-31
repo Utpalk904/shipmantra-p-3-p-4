c7ec49df6701a1e72942aeda0fe307b77a3a3970
Utpal <utpalk904@gmail.com>
2023-03-31 15:03:12 +0530

    frontend separated

--- backend/routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createProduct, getAllProducts } = require('../controllers/productController');
-
-const router = express.Router();
-
-router.route('/product/new').post(createProduct);
-router.route('/products').get(getAllProducts);
-
-module.exports = router;
